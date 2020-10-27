2347c60dd1ef554c4636ee6b78881bde6ddc7984
Mattias Didriksson <mattias.didriksson@gmail.com>
2020-10-27 22:10:37 +0100

    UI är svårt...!

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -3,12 +3,19 @@ module Main exposing (..)
 import Browser
 import Debug exposing (log)
 import Dict exposing (Dict)
-import Html exposing (Attribute, Html, button, div, header, img, input, li, p, span, text, ul)
-import Html.Attributes exposing (..)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font exposing (alignLeft)
+import Element.Input as Input
+import Html exposing (Attribute, Html, button, div, header, img, input, li, p, span, ul)
+import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
 import Http exposing (request)
 import Json.Decode as D
 import Maybe exposing (Maybe)
+import Regex
 
 
 
@@ -24,13 +31,20 @@ main =
 
 
 type Model
-    = InputMatches (Dict Int AnalyzeRequest)
+    = InputRequestState AnalyzeInput
     | Loading
     | Success (List AnalyzeResult)
     | Failure
 
 
-type alias AnalyzeRequest =
+type alias AnalyzeInput =
+    { matches : Dict Int MatchInputBox
+    , helgarderingar : String
+    , halvgarderingar : String
+    }
+
+
+type alias MatchInputBox =
     { home : String
     , away : String
     }
@@ -45,23 +59,26 @@ type alias AnalyzeResult =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( InputMatches
-        (Dict.fromList
-            [ ( 0, AnalyzeRequest "" "" )
-            , ( 1, AnalyzeRequest "" "" )
-            , ( 2, AnalyzeRequest "" "" )
-            , ( 3, AnalyzeRequest "" "" )
-            , ( 4, AnalyzeRequest "" "" )
-            , ( 5, AnalyzeRequest "" "" )
-            , ( 6, AnalyzeRequest "" "" )
-            , ( 7, AnalyzeRequest "" "" )
-            , ( 8, AnalyzeRequest "" "" )
-            , ( 9, AnalyzeRequest "" "" )
-            , ( 10, AnalyzeRequest "" "" )
-            , ( 11, AnalyzeRequest "" "" )
-            , ( 12, AnalyzeRequest "" "" )
-            , ( 13, AnalyzeRequest "" "" )
-            ]
+    ( InputRequestState
+        (AnalyzeInput
+            (Dict.fromList
+                [ ( 0, MatchInputBox "" "" )
+                , ( 1, MatchInputBox "" "" )
+                , ( 2, MatchInputBox "" "" )
+                , ( 3, MatchInputBox "" "" )
+                , ( 4, MatchInputBox "" "" )
+                , ( 5, MatchInputBox "" "" )
+                , ( 6, MatchInputBox "" "" )
+                , ( 7, MatchInputBox "" "" )
+                , ( 8, MatchInputBox "" "" )
+                , ( 9, MatchInputBox "" "" )
+                , ( 10, MatchInputBox "" "" )
+                , ( 11, MatchInputBox "" "" )
+                , ( 12, MatchInputBox "" "" )
+                ]
+            )
+            "0"
+            "0"
         )
     , Cmd.none
     )
@@ -74,87 +91,141 @@ init _ =
 type Msg
     = Changehome Int String
     | Changeaway Int String
+    | Changehelgarderingar String
+    | Changehalvgarderingar String
     | AddMatch
     | SendAnalyzeRequest
     | GotAnalyzeResult (Result Http.Error (List AnalyzeResult))
 
 
-
-{- 
-| GotAnalyzeResult (Result Http.Error (List AnalyzeResult))
--}
-
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Changehome index newContent ->
-      case model of
-        InputMatches matches ->
-          let
-              updateChange =
-                  Maybe.map (\data -> { data | home = newContent })
+    case msg of
+        Changehome index newContent ->
+            case model of
+                InputRequestState inputstate ->
+                    let
+                        updateChange =
+                            Maybe.map (\data -> { data | home = newContent })
 
-              changesUpdated =
-                  Dict.update index
-                      updateChange
-                      matches
-          in
-          ( InputMatches changesUpdated, Cmd.none )
-        _ ->
-          (model, Cmd.none) 
+                        changesUpdated =
+                            Dict.update index
+                                updateChange
+                                inputstate.matches
+                    in
+                    ( InputRequestState { inputstate | matches = changesUpdated }, Cmd.none )
 
-    Changeaway index newContent ->
-      case model of
-        InputMatches matches ->
-          let
-            updateChange =
-              Maybe.map (\data -> { data | away = newContent })
+                _ ->
+                    ( model, Cmd.none )
 
-            changesUpdated =
-                  Dict.update index
-                      updateChange
-                      matches
-          in
-          ( InputMatches changesUpdated, Cmd.none )
-        _ ->
-          (model, Cmd.none) 
+        Changeaway index newContent ->
+            case model of
+                InputRequestState inputstate ->
+                    let
+                        updateChange =
+                            Maybe.map (\data -> { data | away = newContent })
 
-    AddMatch ->
-      case model of
-        InputMatches matches ->
-          let
-              itemsAdded =
-                  Dict.insert (Dict.size matches) (AnalyzeRequest "" "") matches
-          in
-          ( InputMatches itemsAdded, Cmd.none )
-        _ ->
-          (model, Cmd.none) 
+                        changesUpdated =
+                            Dict.update index
+                                updateChange
+                                inputstate.matches
+                    in
+                    ( InputRequestState { inputstate | matches = changesUpdated }, Cmd.none )
 
-    SendAnalyzeRequest ->
-      case model of
-        InputMatches matches ->
-          ( Loading
-          , Http.get
-              { url = "./data.json"
-              , expect = Http.expectJson GotAnalyzeResult analyzeListDecoder
-              }
-          )
-        _ ->
-          (model, Cmd.none) 
+                _ ->
+                    ( model, Cmd.none )
+
+        Changehalvgarderingar newContent ->
+            case model of
+                InputRequestState inputState ->
+                    let
+                        regexNotDigit =
+                            Maybe.withDefault Regex.never <|
+                                Regex.fromString "[^0-9]"
+
+                        containsNonDigit =
+                            Regex.contains regexNotDigit newContent
+                    in
+                    if newContent == "" then
+                        ( InputRequestState { inputState | halvgarderingar = newContent }, Cmd.none )
+
+                    else if containsNonDigit then
+                        ( model, Cmd.none )
+
+                    else if ((String.toInt newContent |> Maybe.withDefault 0) + (String.toInt inputState.helgarderingar |> Maybe.withDefault 0)) > Dict.size inputState.matches then
+                        ( model, Cmd.none )
+
+                    else
+                        ( InputRequestState { inputState | halvgarderingar = newContent }, Cmd.none )
 
 
-    GotAnalyzeResult result ->
-      case model of
-        Loading ->
-          case result of
-              Ok received ->
-                  (Success received, Cmd.none)
-              Err _ ->
-                  (Failure, Cmd.none)
-        _ ->
-          (model, Cmd.none) 
-            
+                _ ->
+                    ( model, Cmd.none )
+
+        Changehelgarderingar newContent ->
+            case model of
+                InputRequestState inputState ->
+                    let
+                        regexNotDigit =
+                            Maybe.withDefault Regex.never <|
+                                Regex.fromString "[^0-9]"
+
+                        containsNonDigit =
+                            Regex.contains regexNotDigit newContent
+                    in
+                    if newContent == "" then
+                        ( InputRequestState { inputState | helgarderingar = newContent }, Cmd.none )
+
+                    else if containsNonDigit then
+                        ( model, Cmd.none )
+
+                    else if ((String.toInt newContent |> Maybe.withDefault 0) + (String.toInt inputState.halvgarderingar |> Maybe.withDefault 0)) > Dict.size inputState.matches then
+                        ( model, Cmd.none )
+
+                    else
+                        ( InputRequestState { inputState | helgarderingar = newContent }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        AddMatch ->
+            case model of
+                InputRequestState inputstate ->
+                    let
+                        itemsAdded =
+                            Dict.insert (Dict.size inputstate.matches) (MatchInputBox "" "") inputstate.matches
+                    in
+                    ( InputRequestState { inputstate | matches = itemsAdded }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        SendAnalyzeRequest ->
+            case model of
+                InputRequestState _ ->
+                    ( Loading
+                    , Http.get
+                        { url = "./data.json"
+                        , expect = Http.expectJson GotAnalyzeResult analyzeListDecoder
+                        }
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        GotAnalyzeResult result ->
+            case model of
+                Loading ->
+                    case result of
+                        Ok received ->
+                            ( Success received, Cmd.none )
+
+                        Err _ ->
+                            ( Failure, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
@@ -169,43 +240,128 @@ subscriptions model =
 -- VIEW
 
 
+header : Element msg
+header =
+    el
+        [ height <| fillPortion 1
+        , width fill
+        , Background.color <| rgb255 92 99 118
+        , Font.color <| rgb255 255 255 255
+        ]
+    <|
+        text "Tipshjälpen"
+
+
+inputMatchesView : AnalyzeInput -> Element Msg
+inputMatchesView matches =
+    column
+        [ height <| fillPortion 5
+        , width fill
+        , centerY
+        , Background.color <| rgb255 200 99 118
+        , paddingEach { top = 20, left = 5, right = 5, bottom = 20 }
+        , spacing 10
+        ]
+    <|
+        List.map inputView (Dict.toList matches.matches)
+
+
+
+{- div
+   []
+   [ header [] [ img [ src "./assets/logo.png" ] [] ]
+   , div []
+       [ div [] (List.map inputView (Dict.toList matches))
+       , button [ onClick AddMatch ] [ text "+" ]
+       , button [ onClick SendAnalyzeRequest ] [ text "Send it!" ]
+       ]
+   ]
+-}
+
+
+inputView : ( Int, MatchInputBox ) -> Element Msg
+inputView ( key, val ) =
+    row
+        [ paddingEach { top = 5, left = 5, right = 20, bottom = 0 }
+        , spacing 20
+        , centerY
+        , height shrink
+        ]
+        [ el [ padding 5, width (px 20) ] <| Element.text (String.fromInt (key + 1))
+        , Input.text
+            [ centerX
+            , centerY
+            ]
+            { text = val.home
+            , onChange = Changehome key
+            , placeholder =
+                Just
+                    (Input.placeholder []
+                        (text "Home")
+                    )
+            , label = Input.labelHidden "Hometeam input"
+            }
+        , Input.text
+            [ centerX
+            , centerY
+            ]
+            { text = val.away
+            , onChange = Changeaway key
+            , placeholder =
+                Just
+                    (Input.placeholder []
+                        (text "Away")
+                    )
+            , label = Input.labelHidden "Awayteam input"
+            }
+        ]
+
+
+mainView : AnalyzeInput -> Element Msg
+mainView input =
+    row [spacing 20, height fill, padding 10]
+        [ inputMatchesView input
+        , Input.text
+            [width shrink]
+            { text = input.helgarderingar
+            , onChange = Changehelgarderingar
+            , placeholder = Nothing
+            , label = Input.labelAbove [] (text "Helgarderingar")
+            }
+        , Input.text
+            [width shrink]
+            { text = input.halvgarderingar
+            , onChange = Changehalvgarderingar
+            , placeholder = Nothing
+            , label = Input.labelAbove [] (text "Halvgarderingar")
+            }
+        ]
+
+
 view : Model -> Html Msg
 view model =
     case model of
-        InputMatches matches ->
-            div []
-                [ header [] [ img [ src "./assets/logo.png" ] [] ]
-                , div []
-                    [ div [] (List.map inputView (Dict.toList matches))
-                    , button [ onClick AddMatch ] [ text "+" ]
-                    , button [ onClick SendAnalyzeRequest ] [ text "Send it!" ]
+        InputRequestState matches ->
+            layout [] <|
+                column [ height fill, width fill ]
+                    [ header
+                    , mainView matches
                     ]
-                ]
+
         Loading ->
-          text "Loading...!"
-        Success results->
-          ul []
-            (List.map (\l -> analyzeView l) results)
+            layout [] <| text "Loading...!"
+
+        Success results ->
+            ul []
+                (List.map (\l -> analyzeView l) results)
 
         Failure ->
-          text "Something is wrong...!"
+            layout [] <| text "Something is wrong...!"
 
 
 analyzeView : AnalyzeResult -> Html Msg
 analyzeView res =
-  li [] [
-    div [] [
-      text res.predictedScore]
-    , text res.hometeam
-    , text "-"
-    , text res.awayteam]
-
-inputView : ( Int, AnalyzeRequest ) -> Html Msg
-inputView ( key, val ) =
-    li []
-        [ input [ placeholder "Hometeam", value val.home, onInput (Changehome key) ] []
-        , input [ placeholder "Awayteam", value val.away, onInput (Changeaway key) ] []
-        ]
+    div [] []
 
 
 analyzeDecoder : D.Decoder AnalyzeResult
