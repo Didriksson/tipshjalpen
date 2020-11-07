f01b3df3b3c68d16451ef55402e447a1bd9a9cfa
Mattias Didriksson <mattias.didriksson@gmail.com>
2020-11-07 13:30:44 +0100

    Bytt datakälla till ett bättre API. Begränsat till 10 anrop i timmen dock.

--- backend/src/englandRepository.py
+++ backend/src/englandRepository.py
@@ -1,29 +1,9 @@
-import requests
-import json
-import match
-
-# https://github.com/openfootball/football.json/tree/master/2020-21
+import footballdataService
 
 
 def fetch_Ch_2020_21():
-    data = fetch_data("https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/en.2.json")
-    return parse_json(data)
+    return footballdataService.getMatchesForLeague('ELC')
 
 def fetch_PL_2020_21():
-    data = fetch_data("https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/en.1.json")
-    return parse_json(data)
+    return footballdataService.getMatchesForLeague('PL')
 
-def fetch_data(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Could not fetch data.", response.status_code)    
-        exit(1)
-
-    return json.loads(response.content)
-
-
-def parse_json(json):
-    matches = []
-    for m in json['matches']:
-        matches.append(match.Match(m.get("date"), m.get("team1"), m.get("team2"), m.get("score", {}).get("ft"), m.get("score", {}).get("ht"), json["name"]))
-    return matches
---
+++ backend/src/footballdataService.py
@@ -0,0 +1,24 @@
+import http.client
+import match
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+FD_API_KEY = os.environ.get('FOOTBALL_DATA_KEY')
+
+def getMatchesForLeague(league):
+    print("Hämtar för liga:" + league)
+    connection = http.client.HTTPConnection('api.football-data.org')
+    headers = { 'X-Auth-Token': FD_API_KEY }
+    connection.request('GET', '/v2/competitions/'+league+"/matches", None, headers )
+    response = json.loads(connection.getresponse().read().decode())
+    return parseJson(response)
+
+def parseJson(matchesJson):
+    matches = []
+    competition = matchesJson['competition']['name']
+    for m in matchesJson['matches']:
+        if m['status'] == 'FINISHED':
+            matches.append(match.Match(m['utcDate'], m['homeTeam']['name'], m['awayTeam']['name'], [m['score']['fullTime']['homeTeam'], m['score']['fullTime']['awayTeam']], [m['score']['halfTime']['homeTeam'], m['score']['halfTime']['awayTeam']], competition))
+    return matches
